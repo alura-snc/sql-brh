d5f073cdc5f31f036411094b2ad38559b9aac64f
Anderson Andrade <anderson.carloscarvalho@gmail.com>
2023-09-23 21:53:40 -0300

    Cadastrando o novo colaborador Fulano de Tal no novo projeto BI para exercer o papel de Especialista de Negócios.

---
+++ sql/comandos.sql
@@ -0,0 +1,46 @@
+INSERT INTO BRH.ENDERECO (cep, uf, cidade, bairro)
+VALUES
+('02401-200','SP','São Paulo','Santana');
+
+INSERT INTO BRH.COLABORADOR (matricula, cpf, nome, salario, departamento, cep,
+logradouro, complemento_endereco) 
+VALUES 
+('A124','335.578.528.06','fulano de tal',580.55,'DEPTI','02401-200','Rua Voluntários da Pátria','Senac');
+
+INSERT INTO BRH.TELEFONE_COLABORADOR (numero, colaborador, tipo)
+VALUES
+('(61) 9 9999-9999','A124','M');
+
+INSERT INTO BRH.TELEFONE_COLABORADOR (numero, colaborador, tipo)
+VALUES
+('(61) 3030-4040','A124','R');
+
+INSERT INTO BRH.EMAIL_COLABORADOR (email, colaborador, tipo)
+VALUES
+('fulano@email.com','A124','P');
+
+INSERT INTO BRH.EMAIL_COLABORADOR (email, colaborador, tipo)
+VALUES
+('fulano.tal@brh.com','A124','T');
+
+INSERT INTO BRH.DEPENDENTE (cpf, nome, data_nascimento, parentesco, colaborador)
+VALUES
+('236.254.875-55','Beltrana de Tal','02/02/2022','Filho(a)','A124');
+
+INSERT INTO BRH.DEPENDENTE (cpf, nome, data_nascimento, parentesco, colaborador)
+VALUES
+('005.275.852-98','Cicrana de Tal','01/08/1785','CÃ´njuge','A124');
+
+INSERT INTO BRH.PROJETO (nome,responsavel, inicio, fim) --ID GERADO AUTOMÁTICAMENTE
+VALUES
+('BI','B123','01/09/2023','12/12/2023');
+
+INSERT INTO BRH.PAPEL (nome) --ID GERADO AUTOMÁTICAMENTE
+VALUES
+('Especialista de Negócios');
+
+INSERT INTO BRH.ATRIBUICAO (colaborador, projeto,papel)
+VALUES
+('A124',9,8);
+
+SELECT * FROM BRH.ATRIBUICAO;
