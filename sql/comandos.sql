63d9006dbecf379b3a8c9e84cbc2f1204ebdf7e6
MarceloMFreitas <143090673+MarceloMFreitas@users.noreply.github.com>
2023-09-26 13:47:32 -0300

    Update comandos.sql

--- sql/comandos.sql
+++ sql/comandos.sql
@@ -50,7 +50,12 @@ on co.matricula = dp.colaborador
 group by co.nome;
 
 
-
+select
+cpf, nome, to_char(data_nascimento, 'dd/mm/yyy') as "Data_nascimento", parentesco, colaborador, trunc(months_between(sysdate,data_nascimento)/12) as "idade",
+(case when months_between(sysdate,data_nascimento)/12 < 18 then 'Menor de idade'
+      when months_between(sysdate,data_nascimento)/12 >=18  then 'Maior de idade' end) as Faixa_etaria
+from
+brh.dependente;
 
 
 
