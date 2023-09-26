22abd3b6a019988b8b8bcbf1c4c7697381062567
MarceloMFreitas <143090673+MarceloMFreitas@users.noreply.github.com>
2023-09-26 09:42:21 -0300

    Update comandos.sql

--- sql/comandos.sql
+++ sql/comandos.sql
@@ -1,4 +1,11 @@
 
+select nome, data_nascimento, to_char(data_nascimento,' month') from brh.dependente
+where(extract(month from data_nascimento)>=4 and extract(month from data_nascimento)<=6 )or( nome like '%h%' or nome like 'H%');
+
+select nome, salario from brh.colaborador
+order by salario desc;
+
+
 
 select
 co.matricula, co.nome, co.salario,
