e142c9b091fb5e6536563404356cc95b7bfbd0b0
Anderson Andrade <anderson.carloscarvalho@gmail.com>
2023-09-29 21:05:35 -0300

    Criando uma consulta com o nome do departamento, chefe, colaborador, projeto que ele está alocado, seu papel no projeto, seu telefone, dependente e ordenando por projeto, colaborador, dependente

--- sql/comandos.sql
+++ sql/comandos.sql
@@ -31,11 +31,11 @@ INSERT INTO BRH.DEPENDENTE (cpf, nome, data_nascimento, parentesco, colaborador)
 VALUES
 ('005.275.852-98','Cicrana de Tal','01/08/1785','C√É¬¥njuge','A124');
 
-INSERT INTO BRH.PROJETO (nome,responsavel, inicio, fim) --ID GERADO AUTOM√ÅTICAMENTE
+INSERT INTO BRH.PROJETO (nome,responsavel, inicio, fim) --ID GERADO AUTOM√?TICAMENTE
 VALUES
 ('BI','B123','01/09/2023','12/12/2023');
 
-INSERT INTO BRH.PAPEL (nome) --ID GERADO AUTOM√ÅTICAMENTE
+INSERT INTO BRH.PAPEL (nome) --ID GERADO AUTOM√?TICAMENTE
 VALUES
 ('Especialista de Neg√≥cios');
 
@@ -51,7 +51,8 @@ SELECT sigla, nome as "NOME DO DEPARTAMENTO" FROM BRH.DEPARTAMENTO ORDER BY (nom
 
 SELECT C.nome AS "NOME DO COLABORADOR", D.nome AS "NOME DO DEPENDENTE",
 D.data_nascimento AS "DATA DE NASCIMENTO DO DEPENDENTE", D.parentesco  
-FROM BRH.COLABORADOR C JOIN BRH.DEPENDENTE D ON  D.COLABORADOR = C.MATRICULA;
+FROM BRH.COLABORADOR C JOIN BRH.DEPENDENTE D ON  D.COLABORADOR = C.MATRICULA
+order by c.nome, d.nome;
 
 --Removendo registro das tabelas Colaboradores e Departamentos e relacionamentos
 
@@ -76,32 +77,39 @@ JOIN BRH.EMAIL_COLABORADOR E ON  E.COLABORADOR = C.MATRICULA AND E.TIPO = 'T'
 JOIN BRH.TELEFONE_COLABORADOR T ON T.COLABORADOR = C.MATRICULA AND T.TIPO = 'M'
 ORDER BY (C.NOME);
 
+-- Crie uma consulta que liste:
+    --O nome do Departamento;
+    --O nome do chefe do Departamento;
+    --O nome do Colaborador;
+    --O nome do Projeto que ele est· alocado;
+    --O nome do papel desempenhado por ele;
+    --O n˙mero de telefone do Colaborador;
+    --O nome do Dependente do Colaborador.
+    --ordenado pelo nome do nome do projeto, nome do colaborador e nome do dependente.
 
--- verificar  o select abaixo
-
-SELECT
-    D.nome AS "Departamento",
-    D.chefe AS "Chefe do Departamento",
-    C.nome AS "Colaborador",
-    P.nome AS "Projeto",
-    PA.nome AS "Nome do Papel",
-    TO_CHAR(TC.numero) AS "Telefone",
-    DE.nome AS "Dependente"
-FROM
-    C##brh.DEPARTAMENTO D
-JOIN
-    C##brh.COLABORADOR C ON D.sigla = C.departamento
-LEFT JOIN
-    C##brh.PROJETO P ON C.matricula = P.responsavel
-LEFT JOIN
-    C##brh.PAPEL PA ON C.matricula = TO_CHAR(PA.id)
-LEFT JOIN
-    C##brh.TELEFONE_COLABORADOR TC ON C.matricula = TC.colaborador
-LEFT JOIN
-    C##brh.DEPENDENTE DE ON C.matricula = DE.colaborador
-ORDER BY
-    "Projeto", "Colaborador", "Dependente";
-
-
-
-
+SELECT 
+    D.NOME AS DEPARTAMENTO, 
+    CF.NOME AS "CHEFE DO DEPARTAMENTO", 
+    C.NOME AS COLABORADOR,
+    P.NOME AS "NOME DO PROJETO",
+    PL.NOME AS "PAPEL DO COLABORADOR",
+    T.NUMERO AS "TELEFONE DO COLABORADOR",
+    DC.NOME AS "DEPENDENTE DO COLABORADOR"
+FROM BRH.DEPARTAMENTO D
+INNER JOIN
+    BRH.COLABORADOR C ON C.DEPARTAMENTO = D.SIGLA
+INNER JOIN
+    BRH.COLABORADOR CF ON CF.MATRICULA = D.CHEFE  AND CF.DEPARTAMENTO = D.SIGLA
+INNER JOIN
+    BRH.ATRIBUICAO A ON A.COLABORADOR = C.MATRICULA
+INNER JOIN
+    BRH.PROJETO P ON P.ID = A.PROJETO
+INNER JOIN
+    BRH.PAPEL PL ON PL.ID = A.PAPEL
+INNER JOIN
+    BRH.TELEFONE_COLABORADOR T ON T.COLABORADOR = C.MATRICULA
+INNER JOIN
+    BRH.DEPENDENTE DC ON DC.COLABORADOR = C.MATRICULA
+WHERE
+    T.TIPO ='M'
+ORDER BY P.NOME,C.NOME,DC.NOME;
