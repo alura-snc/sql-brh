3a558434a559c8e77793fb8876219bf3cd8bbbaf
RicKaplan <kaplan.richard@gmail.com>
2023-10-05 20:11:25 -0300

    semana_04_02

--- sql/plsql.sql
+++ sql/plsql.sql
@@ -5,20 +5,18 @@
   --- data           autor           detalhamento
   --- ---------- --------------- -----------------------------------
   --- 03/10/2023   Kaplan          Construcao da procedure
-  create or replace procedure insere_projeto(p_nome in varchar,
+create or replace procedure insere_projeto(p_nome in varchar,
                                                  p_responsavel in varchar,
                                                  p_retorno out varchar) is
   -- variaveis 
   v_data_atual   date := sysdate;
- 
+
 begin
-           p_retorno := 'Falha';
   -------------------------------
   -- valida parametros de entrada
   -------------------------------
-  
-    if p_nome is null then
-      p_retorno := 'Nome do Projeto Inválido';
+    if p_nome is null or length(p_nome) < 2  then
+      p_retorno := 'Nome de projeto inválido! Deve ter dois ou mais caracteres.';
     else
         if p_responsavel is null then
            p_retorno := 'Responsável do Projeto Inválido';
@@ -30,13 +28,14 @@ begin
            p_retorno := 'Ok';
         end if;	
     end if;		
- return;
+
   -- log de execucao
   dbms_output.put_line(' Projeto Inserido: Nome: ' || p_nome || ' Responsavel: ' || p_responsavel || ' Inicio : ' || to_date(v_data_atual,'DD/MM/RR'));
   dbms_output.put_line(' Finalizado com sucesso');
 exception
   when others then
     dbms_output.put_line(sqlerrm); -- Testes
+    p_retorno := 'Falha';
 end insere_projeto;
 /
   --- nome       : brh.calcula_idade
@@ -67,3 +66,26 @@ from dual;
 return v_retorna_idade; 
 end calcula_idade;
 /
+  --- nome       : brh.finaliza_projeto
+  --- objetivo   : finalizar projeto com a data corrente
+  --- historico  :
+  ---
+  --- data           autor           detalhamento
+  --- ---------- --------------- -----------------------------------
+  --- 05/10/2023   Kaplan          Construcao da function
+create or replace function brh.finaliza_projeto ( 
+p_id_projeto number) 
+return date is 
+
+  -- variaveis 
+  v_data_atual   date := sysdate;
+
+begin 
+
+  ----------------------------------------
+  -- finaliza projeto com a data corrente
+  ----------------------------------------
+update projeto set fim = v_data_atual where id = p_id_projeto;
+
+return v_data_atual; 
+end finaliza_projeto;
