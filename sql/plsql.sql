7ccaaef1f1afd1f4b7064485accbb02a4b72e0ff
Renato Rodrigues <renato.rojunior@gmail.com>
2023-10-09 20:09:17 -0300

    Criar função calcula_idade.

--- sql/plsql.sql
+++ sql/plsql.sql
@@ -11,6 +11,19 @@ BEGIN
     VALUES (p_id_projeto, p_nome_projeto, p_responsavel_projeto, SYSDATE, NULL);
 END;
 
+-- Criar função calcula_idade
+CREATE OR REPLACE FUNCTION calcula_idade(
+    p_data_referencia IN DATE
+) 
+RETURN INTEGER
+IS
+    v_idade INTEGER;
+BEGIN
+    v_idade := TRUNC(MONTHS_BETWEEN(SYSDATE, p_data_referencia) / 12);
+    RETURN v_idade;
+END calcula_idade;
+
+SELECT calcula_idade(TO_DATE('18-02-1993', 'DD-MM-YYYY')) AS idade FROM DUAL; -- Testar Função
 
 
 
