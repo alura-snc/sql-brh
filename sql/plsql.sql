6b8fc68c142acd02602402745b12551ccb9bd0af
Arley <docente.arley@gmail.com>
2023-10-09 11:55:57 -0300

    Criação da função calcular idade

--- sql/plsql.sql
+++ sql/plsql.sql
@@ -14,6 +14,19 @@ end;
 execute sp_inserirProjeto('Saúde PMSP','11122',to_date('07/11/2023','DD/MM/YYYY'));
 commit;
 
+--Criar função calcula_idade
+
+create or replace function fc_calcularIdade(p_data in date)
+return numeric
+is
+v_idade integer;
+begin
+select TRUNC(months_between(SYSDATE, p_data)/12,0) into v_idade from dual;
+return v_idade;
+end;
+
+select fc_calcularidade(to_date('9/10/1979','DD/MM/YYYY')) as "IDADE" from dual;
+
 
 
 
