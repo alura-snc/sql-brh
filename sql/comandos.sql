8ecd7b4c54ad93f515556b24460ba5209ebbf64c
Renato Rodrigues <renato.rojunior@gmail.com>
2024-01-18 20:12:31 -0300

    Inserindo novo colaborador.

---
+++ sql/comandos.sql
@@ -0,0 +1,30 @@
+-- Novo Colaborador
+INSERT INTO brh.colaborador (matricula, nome, cpf, salario, departamento, cep, logradouro, complemento_endereco) 
+VALUES ('A124', 'Guilherme', '368.174.850-02', '10000.00', 'DEPTI', '11015-003', 'Av. Conselheiro Nébias', '309');
+
+-- Novos Telefones: Celular ('M'), Residencial ('R') e Corporativo ('C').
+INSERT INTO brh.telefone_colaborador (colaborador, numero, tipo) 
+VALUES ('A124', '(13) 97483-7325', 'M');
+INSERT INTO brh.telefone_colaborador (colaborador, numero, tipo) 
+VALUES ('A124', '(13) 3361-2052', 'R');
+INSERT INTO brh.telefone_colaborador (colaborador, numero, tipo) 
+VALUES ('A124', '(13) 2105-7764', 'C');
+
+-- Novos E-mails: Particular ('P') e Corporativo ('T').
+INSERT INTO brh.email_colaborador (colaborador, email, tipo) 
+VALUES ('A124', 'guilherme@email.com', 'P');
+INSERT INTO brh.email_colaborador (colaborador, email, tipo) 
+VALUES ('A124', 'guilherme@corporativo.com', 'T');
+
+-- Dependentes
+INSERT INTO brh.dependente (cpf, colaborador, nome, parentesco, data_nascimento) 
+VALUES ('373.554.600-56', 'A124', 'Adelaide', 'Cônjuge', to_date('1991-11-10', 'yyyy-mm-dd'));
+INSERT INTO brh.dependente (cpf, colaborador, nome, parentesco, data_nascimento) 
+VALUES ('575.362.630-07', 'A124', 'Luiza', 'Filho(a)', to_date('2018-06-02', 'yyyy-mm-dd'));
+
+-- Novo Projeto
+INSERT INTO brh.projeto (id, nome, responsavel, inicio, fim) 
+VALUES (5, 'BI', 'A124', to_date('2023-09-25', 'yyyy-mm-dd'), null);
+
+-- Papel do Colaborador no Projeto BI
+INSERT INTO brh.atribuicao (projeto, colaborador, papel) VALUES (5, 'A124', 1);
